2e3e27d6b3e5e2ed90492ebf2d8852387a1f9559
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-09-12 12:06:52 -0500

    Working version. Takes secret, gives alert when secret is received and stored by NSUserDefaults.

--- TellMeASecret/ViewController.swift
+++ TellMeASecret/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var secretLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //if there's a user secret, load it into the label, else keep the label hidden
+        if let labelString = UserDefaults.standard.object(forKey: "userSecret") as? String {
+            secretLabel.text = "You told me: \(labelString). Don't worry, it's still our little secret ;)"
+            secretLabel.isHidden = false
+        } else {
+            secretLabel.isHidden = true
+        }
     }
 
-
+    @IBAction func submitSecret(_ sender: UIButton) {
+        
+        if textView.text != "" {
+            guard let secret = textView.text else { return }
+            UserDefaults.standard.set(secret, forKey: "userSecret")
+            
+            let alert = UIAlertController(title: "Secret received", message: "Don't worry, it's just between you and me!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "It'd better be!", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
 }
 
